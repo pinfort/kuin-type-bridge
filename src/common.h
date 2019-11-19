708f237f4273e8e40f8b3541544f6872aefc91e4
pinfort <ptg@nijitei.com>
2019-11-19 16:10:06 +0900

    調整 include

--- src/common.h
+++ src/common.h
@@ -3,8 +3,4 @@
 #pragma comment(lib, "ucrt.lib")
 #pragma comment(lib, "vcruntime.lib")
 
-#include <Windows.h>
-#include "stdio.h"
 #include <string>
-
-//static const unsigned long long DefaultRefCntFunc = 0; // Just before exiting the function, this is incremented for 'GcInstance'.
--- src/convert.h
+++ src/convert.h
@@ -1,4 +1,6 @@
 #pragma once
 
+#include <string>
+
 std::wstring KuinStrToWStr(const unsigned char* kuin_str);
 unsigned char* WStrToKuinStr(const std::wstring wstr);
